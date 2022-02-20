93c9c232a9131d21f35455fd487ce360b1208f37
constantine <fribbleart@gmail.com>
2022-02-20 11:17:07 +0800

    EAdded paginate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "net/http"
+
+url = "https://picsum.photos/v2/list?page=2&limit=100"
+resp = Net::HTTP.get_response(URI.parse(url))
+data = JSON.parse(resp.body)
+
+data.each do |d|
+  url = "https://picsum.photos/200/#{300+(rand(1..5)*20)}"
+  p Image.where(author: d['author'], url: url).first_or_create
+end
