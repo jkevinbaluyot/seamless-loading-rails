1dbb5ca52556830362f335b93a31061c392f31f5
constantine <fribbleart@gmail.com>
2022-04-15 17:05:38 +0800

    Edited schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_02_12_040238) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: :cascade do |t|
     t.string "url"
     t.string "author"
